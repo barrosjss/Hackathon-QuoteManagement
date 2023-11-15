5ca04f46c889cb275da0f80792507c4086f11b0d
Notonyus_risk <antoniojoserudassanchez@gmail.com>
2023-11-15 11:25:57 -0500

    change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import SignInSide from "./pages/SignIn"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <>
       <SignInSide />
